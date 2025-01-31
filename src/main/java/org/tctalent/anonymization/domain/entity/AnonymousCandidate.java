5e887a3f2aff24d8cd384d52ffcc293c9d5e07b1
sadatmalik <sadat.malik@me.com>
2025-01-31 16:42:20 +0000

    dummy anonymous candidate for prototyping end-to-end with aurora

--- src/main/java/org/tctalent/anonymization/domain/entity/AnonymousCandidate.java
+++ src/main/java/org/tctalent/anonymization/domain/entity/AnonymousCandidate.java
@@ -1,28 +1,28 @@
 package org.tctalent.anonymization.domain.entity;
 
 import jakarta.persistence.*;
+import java.time.OffsetDateTime;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
+import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDateTime;
+import org.tctalent.anonymization.entity.db.AbstractDomainObject;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @Entity
 @Table(name = "anonymous_candidate")
-public class AnonymousCandidate {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+@SequenceGenerator(name = "seq_gen", sequenceName = "anonymous_candidate_id_seq", allocationSize = 1)
+@Slf4j
+public class AnonymousCandidate extends AbstractDomainObject<Long> {
 
   @Column(nullable = false, unique = true)
   private String publicId;
 
-  @CreationTimestamp
-  @Column(nullable = false, updatable = false)
-  private LocalDateTime createdDate;
+  @Column(name = "created_date")
+  private OffsetDateTime createdDate;
+
+  @Column(name = "updated_date")
+  private OffsetDateTime updatedDate;
+
 }
