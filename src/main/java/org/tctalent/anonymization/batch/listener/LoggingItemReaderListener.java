471e7b916a5d26c13d80b50f2675655459b4c604
sadatmalik <sadat.malik@me.com>
2025-01-31 17:14:09 +0000

    batch listener package

--- src/main/java/org/tctalent/anonymization/batch/listener/LoggingItemReaderListener.java
+++ src/main/java/org/tctalent/anonymization/batch/listener/LoggingItemReaderListener.java
@@ -1,4 +1,4 @@
-package org.tctalent.anonymization.batch;
+package org.tctalent.anonymization.batch.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ItemReadListener;
