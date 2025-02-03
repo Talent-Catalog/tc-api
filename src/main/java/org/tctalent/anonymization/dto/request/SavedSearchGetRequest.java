633da7358fff84456d5fe7113c278241f1af9041
sadatmalik <sadat.malik@me.com>
2025-02-03 20:02:02 +0000

    dtos for tc rest api requests

---
+++ src/main/java/org/tctalent/anonymization/dto/request/SavedSearchGetRequest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tctalent.anonymization.dto.request;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.tctalent.anonymization.domain.common.ReviewStatus;
+
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class SavedSearchGetRequest extends PagedSearchRequest {
+
+    /**
+     * Candidates with search review statuses matching the statuses in this list will be excluded
+     * from the search results.
+     */
+    private List<ReviewStatus> reviewStatusFilter;
+}
