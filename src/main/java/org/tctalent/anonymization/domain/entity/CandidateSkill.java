fe5a1e701936beacda176e83aa7e915140d5c665
sadatmalik <sadat.malik@me.com>
2025-02-03 16:15:44 +0000

    candidate skill doc and entity done

--- src/main/java/org/tctalent/anonymization/domain/entity/CandidateSkill.java
+++ src/main/java/org/tctalent/anonymization/domain/entity/CandidateSkill.java
@@ -14,7 +14,7 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
-package org.tctalent.anonymization.entity.db;
+package org.tctalent.anonymization.domain.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -22,7 +22,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.tctalent.anonymization.entity.db.AbstractDomainObject;
+import org.tctalent.anonymization.entity.db.Candidate;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "candidate_skill")
 @SequenceGenerator(name = "seq_gen", sequenceName = "candidate_skill_id_seq", allocationSize = 1)
@@ -31,31 +37,8 @@ public class CandidateSkill extends AbstractDomainObject<Long> {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "candidate_id")
     private Candidate candidate;
+
     private String skill;
     private String timePeriod;
-
-    public Candidate getCandidate() {
-        return candidate;
-    }
-
-    public void setCandidate(Candidate candidate) {
-        this.candidate = candidate;
-    }
-
-    public String getSkill() {
-        return skill;
-    }
-
-    public void setSkill(String skill) {
-        this.skill = skill;
-    }
-
-    public String getTimePeriod() {
-        return timePeriod;
-    }
-
-    public void setTimePeriod(String timePeriod) {
-        this.timePeriod = timePeriod;
-    }
 
 }
