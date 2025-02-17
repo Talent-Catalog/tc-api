f46d8594d633f34d609a3cacca36fa1c98928c9d
sadatmalik <sadat.malik@me.com>
2025-02-17 13:50:50 +0000

    batch failed item entity and jpa repository

---
+++ src/main/java/org/tctalent/anonymization/domain/entity/BatchFailedItemEntity.java
@@ -0,0 +1,39 @@
+package org.tctalent.anonymization.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "batch_failed_items")
+@SequenceGenerator(name = "seq_gen", sequenceName = "candidate_certification_id_seq", allocationSize = 1)
+@NoArgsConstructor
+@AllArgsConstructor
+public class BatchFailedItemEntity extends AbstractDomainEntity<Long>{
+
+  @Column(name = "step_execution_id", nullable = false)
+  private Long stepExecutionId;
+
+  @Column(name = "step_name", length = 100)
+  private String stepName;
+
+  @Column(name = "item_type", length = 50)
+  private String itemType;
+
+  @Column(name = "item_public_id", length = 100)
+  private String itemPublicId;
+
+  @Column(name = "error_phase", length = 20)
+  private String errorPhase;
+
+  @Column(name = "error_message")
+  private String errorMessage;
+
+}
