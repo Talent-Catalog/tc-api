0289bd11aade9fdd09cdc1dcd5e3ed803c9efc90
sadatmalik <sadat.malik@me.com>
2025-02-17 15:08:52 +0000

    correct flyways - types and column names

--- src/main/resources/db/migration/V1_31__create_batch_failed_items_table.sql
+++ src/main/resources/db/migration/V1_31__create_batch_failed_items_table.sql
@@ -1,7 +1,7 @@
 -- V2__create_batch_failed_items_table.sql
 
 CREATE TABLE batch_failed_items (
-    id SERIAL PRIMARY KEY,
+    id BIGINT PRIMARY KEY,
     job_execution_id BIGINT NOT NULL,
     step_execution_id BIGINT NOT NULL,
     step_name VARCHAR(100),
@@ -9,6 +9,6 @@ CREATE TABLE batch_failed_items (
     item_public_id TEXT,            -- e.g. a single item or multiple public IDs for a failed chunk
     error_phase VARCHAR(20),        -- e.g. "PROCESSING" or "WRITING"
     error_message TEXT,             -- the exception message
-    updated_at TIMESTAMP DEFAULT now(),
-    created_at TIMESTAMP DEFAULT now()
+    updated_date TIMESTAMP DEFAULT now(),
+    created_date TIMESTAMP DEFAULT now()
 );
