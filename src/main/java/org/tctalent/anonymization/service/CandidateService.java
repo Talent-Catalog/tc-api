10539061fc6608e9852ccf64f642aecbbff647f8
John Cameron <john@cameronfoundation.org>
2025-02-25 10:58:51 +1100

    Added some JavaDoc to service

--- src/main/java/org/tctalent/anonymization/service/CandidateService.java
+++ src/main/java/org/tctalent/anonymization/service/CandidateService.java
@@ -1,13 +1,24 @@
 package org.tctalent.anonymization.service;
 
-import java.util.UUID;
 import org.springframework.data.domain.Pageable;
+import org.tctalent.anonymization.exception.NoSuchObjectException;
 import org.tctalent.anonymization.model.Candidate;
 import org.tctalent.anonymization.model.CandidatePage;
 
 public interface CandidateService {
 
+  /**
+   * Returns the requested page of candidates
+   * @param pageable Defines the page required - including page number and size
+   * @return A page of candidates
+   */
   CandidatePage findAll(Pageable pageable);
 
-  Candidate findByPublicId(String publicId);
+  /**
+   * Returns candidate associated with given id
+   * @param publicId Public id
+   * @return Candidate matching id
+   * @throws NoSuchObjectException if no candidate exists with that id
+   */
+  Candidate findByPublicId(String publicId) throws NoSuchObjectException;
 }
