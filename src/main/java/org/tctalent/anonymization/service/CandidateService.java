3f1310870ec3f1ce21fcdfd650bf344cf4c8b92b
John Cameron <john@cameronfoundation.org>
2025-02-26 18:21:52 +1100

    First go at implementing filtering in getAllCandidates

--- src/main/java/org/tctalent/anonymization/service/CandidateService.java
+++ src/main/java/org/tctalent/anonymization/service/CandidateService.java
@@ -1,6 +1,8 @@
 package org.tctalent.anonymization.service;
 
+import java.util.List;
 import org.springframework.data.domain.Pageable;
+import org.springframework.lang.Nullable;
 import org.tctalent.anonymization.exception.NoSuchObjectException;
 import org.tctalent.anonymization.model.Candidate;
 import org.tctalent.anonymization.model.CandidatePage;
@@ -8,11 +10,18 @@ import org.tctalent.anonymization.model.CandidatePage;
 public interface CandidateService {
 
   /**
-   * Returns the requested page of candidates
+   * Returns the requested page of candidates filtered by the values in the given lists if they
+   * are not null. If there are multiple filters they all must be satisfied (ie "and").
    * @param pageable Defines the page required - including page number and size
+   * @param locations List of locations
+   * @param nationalities list of nationalities
+   * @param occupations list of occupations
    * @return A page of candidates
    */
-  CandidatePage findAll(Pageable pageable);
+  CandidatePage findAll(Pageable pageable,
+      @Nullable List<String> locations,
+      @Nullable List<String> nationalities,
+      @Nullable List<String> occupations);
 
   /**
    * Returns candidate associated with given id
--- src/main/java/org/tctalent/anonymization/service/CandidateServiceImpl.java
+++ src/main/java/org/tctalent/anonymization/service/CandidateServiceImpl.java
@@ -1,9 +1,17 @@
 package org.tctalent.anonymization.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+import org.tctalent.anonymization.domain.document.CandidateDocument;
 import org.tctalent.anonymization.exception.NoSuchObjectException;
 import org.tctalent.anonymization.mapper.DocumentMapper;
 import org.tctalent.anonymization.model.Candidate;
@@ -15,14 +23,43 @@ import org.tctalent.anonymization.repository.CandidateDocumentRepository;
 public class CandidateServiceImpl implements CandidateService {
 
   private final CandidateDocumentRepository candidateDocumentRepository;
+  private final MongoTemplate mongoTemplate;
   private final DocumentMapper documentMapper;
 
   @Override
-  public CandidatePage findAll(Pageable pageable) {
-    Page<Candidate> candidatePage =  candidateDocumentRepository
-        .findAll(pageable)
-        .map(documentMapper::toCandidateModel);
+  public CandidatePage findAll(Pageable pageable, List<String> locations,
+      List<String> nationalities, List<String> occupations) {
 
+    Map<String, List<String>> filters = new HashMap<>();
+    if (locations != null) {
+      //TODO JC Fix key
+      filters.put("location", locations);
+    }
+    if (nationalities != null) {
+      //TODO JC Fix key
+      filters.put("nationality", nationalities);
+    }
+    if (occupations != null) {
+      filters.put("candidateOccupations.occupation.isco08Code", occupations);
+    }
+
+    //Construct the basic query from all the filters
+    Query query = new Query();
+    filters.forEach((key, values) -> query
+        .addCriteria(Criteria.where(key).in(values)));
+
+    //Run the query requesting just the page required
+    List<Candidate> candidates = mongoTemplate.find(query.with(pageable), CandidateDocument.class)
+            .stream()
+            .map(documentMapper::toCandidateModel)
+            .toList();
+    //Count the total number of candidates (ie without paging limits).
+    long count = mongoTemplate.count(query, CandidateDocument.class);
+
+    //Construct a page
+    Page<Candidate> candidatePage = new PageImpl<>(candidates, pageable, count);
+
+    //Convert to our CandidatePage structure
     return documentMapper.toCandidateModelPage(candidatePage);
   }
 
