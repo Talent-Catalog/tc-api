fbeb5e82cc4baba814fac9e154d50751a1185d2a
john <jadcpub@gmail.com>
2025-03-06 12:03:22 +1100

    Add simple api key/user lookup caching

--- src/main/java/org/tctalent/anonymization/security/AuthenticationService.java
+++ src/main/java/org/tctalent/anonymization/security/AuthenticationService.java
@@ -4,7 +4,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import lombok.RequiredArgsConstructor;
 import org.springframework.lang.Nullable;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
@@ -21,74 +20,79 @@ import org.tctalent.anonymization.service.TalentCatalogService;
  * context, and then passes the call to the next security filter. Our getAuthentication method is
  * quite simple – we just compare the API Key header and secret with an encrypted database value.
  *
- * @see <a href="https://www.baeldung.com/spring-boot-api-key-secret">
  * @author sadatmalik
+ * @see <a href="https://www.baeldung.com/spring-boot-api-key-secret">
  */
 @Service
-@RequiredArgsConstructor
 public class AuthenticationService {
 
-  /**
-   * Simple cache of apiKeys to ApiUser's - avoiding unnecessary calls back to TC server.
-   * <p/>
-   * Cache is only cleared when server is restarted.
-   */
-  private Map<String, ApiUser> keyToUserCache = new HashMap<>();
+    /**
+     * Simple cache of apiKeys to ApiUser's - avoiding unnecessary calls back to TC server.
+     * <p/>
+     * Cache is only cleared when server is restarted.
+     */
+    private final Map<String, ApiUser> keyToUserCache = new HashMap<>();
 
-  private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
+    private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
 
-  private final TalentCatalogService talentCatalogService;
+    private final TalentCatalogService talentCatalogService;
 
-  public Authentication getAuthentication(HttpServletRequest request) {
-    String presentedApiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
-    if (presentedApiKey == null) {
-      throw new BadCredentialsException("Invalid API Key");
+    public AuthenticationService(TalentCatalogService talentCatalogService) {
+        this.talentCatalogService = talentCatalogService;
     }
 
-    ApiUser apiUser = findApiUserByApiKey(presentedApiKey);
-    if (apiUser == null) {
-      throw new BadCredentialsException("Invalid API key");
+    public Authentication getAuthentication(HttpServletRequest request) {
+        String presentedApiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
+        if (presentedApiKey == null) {
+            throw new BadCredentialsException("Invalid API Key");
+        }
+
+        ApiUser apiUser = findApiUserByApiKey(presentedApiKey);
+        if (apiUser == null) {
+            throw new BadCredentialsException("Invalid API key");
+        }
+
+        return new ApiKeyAuthentication(apiUser);
     }
 
-    return new ApiKeyAuthentication(apiUser);
-  }
+    /**
+     * Looks up user associated with authentication key.
+     *
+     * @param apiKey API key
+     * @return ApiUser matching apiKey or null if there is no match for given key
+     */
+    @Nullable
+    private ApiUser findApiUserByApiKey(String apiKey) {
 
-  /**
-   * Looks up user associated with authentication key.
-   * @param apiKey API key
-   * @return ApiUser matching apiKey or null if there is no match for given key
-   */
-  @Nullable
-  private ApiUser findApiUserByApiKey(String apiKey) {
+        ApiUser apiUser;
 
-    ApiUser apiUser;
+        //Get user from cache if we have already retrieved it
+        if (keyToUserCache.containsKey(apiKey)) {
+            apiUser = keyToUserCache.get(apiKey);
+        } else {
+            //Get user from TC service
+            if (!talentCatalogService.isLoggedIn()) {
+                talentCatalogService.login();
+            }
+            Long partnerId = talentCatalogService.findPartnerIdByPublicApiKey(apiKey);
+            apiUser = partnerId == null ? null : new ApiUser(partnerId);
 
-    //Get user from cache if we have already retrieved it
-    if (keyToUserCache.containsKey(apiKey)) {
-      apiUser = keyToUserCache.get(apiKey);
-    } else {
-      //Get user from TC service
-      if (!talentCatalogService.isLoggedIn()) {
-        talentCatalogService.login();
-      }
-      Long partnerId = talentCatalogService.findPartnerIdByPublicApiKey(apiKey);
-      apiUser = partnerId == null ? null : new ApiUser(partnerId);
-
-      //Remember result in cache. Note that this can store nulls if the key is not recognized.
-      keyToUserCache.put(apiKey, apiUser);
+            //Remember result in cache. Note that this can store nulls if the key is not recognized.
+            keyToUserCache.put(apiKey, apiUser);
+        }
+        return apiUser;
     }
-    return apiUser;
-  }
 
-  /**
-   * Get current ApiUser
-   * @return Current Api user, may be empty
-   */
-  public Optional<ApiUser> getCurrentApiUser() {
-    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-    if (auth != null && auth.getPrincipal() instanceof ApiUser) {
-      return Optional.of((ApiUser) auth.getPrincipal());
+    /**
+     * Get current ApiUser
+     *
+     * @return Current Api user, may be empty
+     */
+    public Optional<ApiUser> getCurrentApiUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.getPrincipal() instanceof ApiUser) {
+            return Optional.of((ApiUser) auth.getPrincipal());
+        }
+        return Optional.empty();
     }
-    return Optional.empty();
-  }
 }
