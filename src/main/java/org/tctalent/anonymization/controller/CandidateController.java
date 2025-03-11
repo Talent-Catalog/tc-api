98d4f4804f3b748d1ae47072b521ce90e2b750b1
John Cameron <john@cameronfoundation.org>
2025-03-11 15:08:42 +1100

    Implement job-match as a special ota

--- src/main/java/org/tctalent/anonymization/controller/CandidateController.java
+++ src/main/java/org/tctalent/anonymization/controller/CandidateController.java
@@ -16,7 +16,12 @@ import org.tctalent.anonymization.domain.entity.ApiUser;
 import org.tctalent.anonymization.dto.request.OfferToAssistRequest;
 import org.tctalent.anonymization.exception.UnauthorisedActionException;
 import org.tctalent.anonymization.model.Candidate;
+import org.tctalent.anonymization.model.CandidateAssistanceType;
+import org.tctalent.anonymization.model.CandidateCoupon;
 import org.tctalent.anonymization.model.CandidatePage;
+import org.tctalent.anonymization.model.JobMatch201Response;
+import org.tctalent.anonymization.model.JobMatchRequest;
+import org.tctalent.anonymization.model.JobMatchRequestCandidatesInner;
 import org.tctalent.anonymization.model.OfferToAssistCandidates201Response;
 import org.tctalent.anonymization.model.OfferToAssistCandidatesRequest;
 import org.tctalent.anonymization.model.RegisterCandidate201Response;
@@ -84,18 +89,70 @@ public class CandidateController implements V1Api {
   public ResponseEntity<OfferToAssistCandidates201Response> offerToAssistCandidates(
       OfferToAssistCandidatesRequest offerToAssistCandidatesRequest) {
 
+    final OfferToAssistCandidates201Response response =
+        doOfferToAssistCandidates(offerToAssistCandidatesRequest);
+
+    return new ResponseEntity<>(response, HttpStatus.CREATED);
+  }
+
+  @Override
+  @PreAuthorize("hasAuthority('SUBMIT_JOB_MATCHES')")
+  public ResponseEntity<JobMatch201Response> jobMatch(JobMatchRequest jobMatchRequest) {
+
+    //JobMatch is just a special kind of OTA.
+    //Convert JobMatchRequest to an equivalent OTA request
+    OfferToAssistCandidatesRequest otaRequest = new OfferToAssistCandidatesRequest();
+    otaRequest.setReason(CandidateAssistanceType.JOB_OPPORTUNITY);
+    otaRequest.setAdditionalNotes(jobMatchRequest.getAdditionalNotes());
+
+    //Job matches just have array of candidate ids - no coupons
+    final List<JobMatchRequestCandidatesInner> candidates = jobMatchRequest.getCandidates();
+
+    //Create array of CandidateCoupons one for each candidate plus a null coupon.
+    final List<CandidateCoupon> candidateCoupons = candidates.stream()
+        .map(c -> new CandidateCoupon(c.getPublicId()))
+        .toList();
+    otaRequest.setCandidates(candidateCoupons);
+
+    final OfferToAssistCandidates201Response otaResponse = doOfferToAssistCandidates(otaRequest);
+
+    //Convert OTA response into a JobMatch response
+    JobMatch201Response response = new JobMatch201Response();
+    response.setJobMatchId(otaResponse.getOfferId());
+    response.setMessage(otaResponse.getMessage());
+
+    return new ResponseEntity<>(response, HttpStatus.CREATED);
+  }
+
+  /**
+   * Send an OfferToAssistCandidates request to the server.
+   * <p/>
+   * Common code used by both {@link #offerToAssistCandidates} and {@link #jobMatch}.
+   * @param offerToAssistCandidatesRequest Request to send
+   * @return Response from server
+   */
+  private OfferToAssistCandidates201Response doOfferToAssistCandidates(
+      OfferToAssistCandidatesRequest offerToAssistCandidatesRequest) {
+
+    //Get the partner id of the public api user
     final Optional<ApiUser> currentApiUser = authenticationService.getCurrentApiUser();
     if (currentApiUser.isEmpty()) {
       throw new UnauthorisedActionException("offerToAssistCandidates");
     }
     long partnerId = currentApiUser.get().getPartner().getPartnerId();
 
+    //Create an internal TC server request from the external request received from the public api user
+    OfferToAssistRequest offerToAssistRequest = new OfferToAssistRequest(
+        offerToAssistCandidatesRequest);
+    //... and add the partner id of that user - retrieved above from the authentication service.
+    offerToAssistRequest.setPartnerId(partnerId);
+
+    //Log in if needed
     if (!talentCatalogService.isLoggedIn()) {
       talentCatalogService.login();
     }
-    OfferToAssistRequest offerToAssistRequest = new OfferToAssistRequest(offerToAssistCandidatesRequest);
-    offerToAssistRequest.setPartnerId(partnerId);
-    return new ResponseEntity<>(talentCatalogService.create(offerToAssistRequest), HttpStatus.CREATED);
+    //...and send the request to the server
+    return talentCatalogService.create(offerToAssistRequest);
   }
 
   @Override
